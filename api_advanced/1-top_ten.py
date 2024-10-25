3b4bd6ea51de75b0fd1ae4d25d0a1efa27104d9c
Phinatech <c.ugbo@alustudent.com>
2024-10-25 05:26:37 -0700

    pushing

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,9 +8,20 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    # Check if the subreddit exists or if there's an error
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+
+    # Parse response safely
+    try:
+        posts = response.json().get('data', {}).get('children', [])
+        if not posts:
+            print(None)
+            return
+        for post in posts:
+            print(post['data'].get('title', ''))
+    except (ValueError, KeyError, TypeError):
+        print(None)
+
