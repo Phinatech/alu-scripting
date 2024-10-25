6a9171063f9897a00ac470bbae31e74cb041bfdb
Phinatech <c.ugbo@alustudent.com>
2024-10-25 05:35:43 -0700

    pushing

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,7 +4,7 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    """Prints the titles of the first 10 hot posts listed in a subreddit."""
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -15,9 +15,12 @@ def top_ten(subreddit):
     # Parse response safely
     try:
         posts = response.json().get('data', {}).get('children', [])
+        
+        # If no posts, simply end function without printing None
         if not posts:
-            print(None)
             return
+            
+        # Print the titles of each post if available
         for post in posts:
             print(post['data'].get('title', ''))
     except (ValueError, KeyError, TypeError):
